7cac80011225e092b691a25efd7f67d719156b04
kristinegroenli-wq <kristinegroenli@gmail.com>
2026-09-13 20:20:05 +0200

    Laget task 3 filen og startet med script

---
+++ Task 3.py
@@ -0,0 +1,15 @@
+#Task 3 - Simple Class and Inheritance
+
+#Lager Person class, bruker samme oppsett som i PRO1002 3.3 Objects
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hi, My name is {self.name} and I am {self.age} years old.")
+
+#Lager student class som arver fra person class
+
+    
+
