d58dfd091161ed09f8fbd537cee50dd5bffb9bc9
kristinegroenli-wq <kristinegroenli@gmail.com>
2026-09-13 20:48:44 +0200

    Hadde glemt .py bak dokumentnavn
    Glemte å sette .py bak dokumentnavn på task 1, så da blir denne slettet ser jeg. Usikker på om jeg får med historikken på den da?

--- Task 3.py
+++ Task 3.py
@@ -9,7 +9,16 @@ class Person:
     def greet(self):
         print(f"Hi, My name is {self.name} and I am {self.age} years old.")
 
-#Lager student class som arver fra person class
+#Lager student class objektet som arver attributter fra person class (eksempel under PRO1002 3.3 Inheritance)
+ 
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        Person.__init__(self, name, age)
+        self.student_id = student_id
 
-    
+student = Student("Kristine", 38, "3001814")
 
+student.greet()
+print(f"Student ID: {student.student_id}")
+
+#printer både greet og student ID. Mulig jeg kunne løst det litt annerledes, og lagt inn alt i samme greet?
