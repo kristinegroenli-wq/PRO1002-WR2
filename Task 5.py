9d4b0418f00e03607d0448ef3959cf9d8036ff3a
kristinegroenli-wq <kristinegroenli@gmail.com>
2026-09-14 16:44:39 +0200

    Created task 5 and started coding

---
+++ Task 5.py
@@ -0,0 +1,21 @@
+#Oppgave 5. Directory lister
+
+import os
+
+path = input ("Directory path:")
+
+try:
+    items = os.listdir(path)
+
+    print(f"\nContent in '{path}':")
+
+
+    for item in items:
+        print(f"-{item}")
+
+except FileNotFoundError:
+    print("Error: File path does not exist")
+
+except PermissionError:
+    print("Error: You do not have permission")
+
